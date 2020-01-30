ae4956cec585d61d283dd6079b428d1a9e1e4069
Yulistiaani <60475102+Yulistiaani@users.noreply.github.com>
2020-01-30 23:09:49 +0700

    project

---
+++ application/views/index.php
@@ -0,0 +1,410 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Head -->
+<head>
+<title>About-me</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="keywords" content="C-Resume a Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!-- css -->
+<!-- font-awesome icons -->
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- //font-awesome icons -->
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:200,300,400,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo base_url();?>asset/css/style.css" type="text/css" media="all" />
+<link rel="stylesheet" href="<?php echo base_url();?>asset/css/bootstrap.min.css" type="text/css" media="all" />
+<!-- Default-JavaScript-File -->
+	<script type="text/javascript" src="<?php echo base_url();?>asset/js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url();?>asset/js/bootstrap.min.js"></script>
+<!-- //Default-JavaScript-File -->
+
+</head>
+<body>
+<!-- banner -->
+	<div class="w3-banner-top">
+	<div class="agileinfo-dot">
+
+		<div class="w3-banner-grids">
+			<div class="col-md-6 w3-banner-grid-left">
+				<div class="w3-banner-img">
+					<img src="<?php echo base_url();?>asset/images/profile.jpg" alt="img">
+					<h3 class="test" style="font-size: 30px">Yulistiani</h3>
+				</div>
+			</div>
+			<div class="col-md-6 w3-banner-grid-right">
+			<div class="w3-banner-text">
+				<h3>Hello!</h3>
+				<p align="justify" style="color: white">Perkenalkan nama saya Yulistiani,anda dapat memanggil saya Yulis. Saya berumur 23 tahun, lulusan sarjana (S1). Saya mudah beradaptasi, dapat bekerja keras, bekerja sama dalam tim, mudah berkomunikasi dengan orang lain dan senang mempelajari hal baru. </p>
+			</div>
+				<div class=" w3-right-addres-1">
+				<ul class="address">
+					<li>
+						<ul class="agileits-address-text ">
+							<li class="agile-it-adress-left"><b>Tempat, tanggal lahir</b></li>
+							<li><span>:</span> Bogor, 24 Januari 1997</li>
+						</ul>
+					</li>
+					<li>
+						<ul class="agileits-address-text">
+							<li class="agile-it-adress-left"><b>Jenis Kelamin</b></li>
+							<li><span>:</span> Perempuan</li>
+						</ul>
+					</li>
+					<li>
+						<ul class="agileits-address-text">
+							<li class="agile-it-adress-left"><b>Agama</b></li>
+							<li><span>:</span> Islam</li>
+						</ul>
+					</li>
+					<li>
+						<ul class="agileits-address-text">
+							<li class="agile-it-adress-left"><b>Kewarganegaraan</b></li>
+							<li><span>:</span> Indonesia</li>
+						</ul>
+					</li>
+					<li>
+						<ul class="agileits-address-text">
+							<li class="agile-it-adress-left"><b>Berat Badan</b></li>
+							<li><span>:</span> 158 cm</li>
+						</ul>
+					</li>
+					<li>
+						<ul class="agileits-address-text">
+							<li class="agile-it-adress-left"><b>Tinggi Badan</b></li>
+							<li><span>:</span> 52 kg</li>
+						</ul>
+					</li>
+					<li>
+						<ul class="agileits-address-text">
+							<li class="agile-it-adress-left"><b>Alamat</b></li>
+							<li><span>:</span> Kp.Loji RT 001/002 Kel.Cileungsi</li>
+						</ul>
+					</li>
+							<li>
+								<ul class="agileits-address-text">
+									<li class="agile-it-adress-left"><b></b></li>
+									<li><span>&nbsp</span> Kec.Ciawi Kab.Bogor 16720</li>
+								</ul>
+							</li>
+				</ul> 
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		</div>
+	</div>
+<!-- banner -->
+<!--/ education -->
+	<div class="w3-edu-top" id="experiences">
+	<div class="container">
+		<div class="w3-edu-grids">
+			<div class="col-md-6 w3-edu-grid-left">
+
+				<!-- Pendidikan -->
+				<div class="w3-edu-grid-header">
+					<h3 style="font-size: 30px">Pendidikan</h3>
+				</div>
+				<div class="col-md-4 w3-edu-info1">
+					<h3>2012 – 2015 </h3>
+				</div>
+				<div class="col-md-6 w3-edu-info2">
+					<h3>SMA Negeri 1 Ciawi</h3>
+				</div>
+				<div class="clearfix"></div>
+				<div class="col-md-4 w3-edu-info1">
+					<h3>2015 - 2019 </h3>	
+				</div>
+				<div class="col-md-6 w3-edu-info2">
+					<h3>Universitas Gunadarma</h3>
+					<p>Teknik Informatika</p>
+				</div>
+				<div class="clearfix"></div>
+				</br></br></br>
+
+				<!-- Organisasi -->
+				<div class="w3-edu-grid-header">
+					<h3 style="font-size: 30px">Pengalaman Berorganisasi</h3>
+				</div>
+				<div class="col-md-4 w3-edu-info1">
+					<h3>2012 - 2013</h3>
+					<h4>OSIS SMAN 1 CIAWI</h4>
+				</div>
+				<div class="col-md-6 w3-edu-info2">
+					<h3>Seksi Bidang 2</h3>
+					<p>Seksi bidang pembinaan budi pekerti luhur atau akhlak mulia.  </p>
+				</div>
+				<div class="col-md-4 w3-edu-info1">
+					<h3>2013 - 2014</h3>
+					<h4>OSIS SMAN 1 CIAWI</h4>
+				</div>
+				<div class="col-md-6 w3-edu-info2">
+					<h3>Mitra Muda 2</h3>
+					<p>Wakil ketua OSIS 2  </p>
+				</div>
+				<div class="col-md-4 w3-edu-info1">
+					<h3>2017 - 2018</h3>
+					<h4>Universitas Gunadarma</h4>
+				</div>
+				<div class="col-md-6 w3-edu-info2">
+					<h3>Asisten Laboratorium</h3>
+					<p>Asisten Laboratorium Fisika Dasar Universitas Gunadarma</p>
+				</div>
+			</div>
+
+			<div class="col-md-6 w3-edu-grid-right">
+				<div class="w3-edu-grid-header">
+					<h3 style="font-size: 30px">Training dan Workshop</h3>
+				</div>
+				<div class="col-md-3 w3-edu-info-right1">
+					<h3>2016 </h3>
+				</div>
+				<div class="col-md-9 w3-edu-info-right2">
+					<h3>Universitas Gunadarma</h3>
+					<p>Fundamental DBMS </p>
+				</div>
+				<div class="clearfix"></div>
+				<div class="col-md-3 w3-edu-info-right1">
+					<h3>2016 </h3>
+				</div>
+				<div class="col-md-9 w3-edu-info-right2">
+					<h3>Universitas Gunadarma</h3>
+					<p>Woocomerce : Empowering Your Blogging and Business experience </p>
+				</div>
+				<div class="clearfix"></div>		
+			</div>
+
+			<div class="col-md-6 w3-edu-grid-left">
+				<!-- Skill -->
+				<div class="w3-edu-grid-header">
+					<h3 style="font-size: 30px">Skill</h3>
+				</div>
+				<div class="col-md-8 w3-edu-info1">
+					<li><h4 style="font-size: 25px">MySQL</h4></li>
+					<li><h4 style="font-size: 25px">Microsoft Office</h4></li>
+					<li><h4 style="font-size: 25px">PHP</h4></li>
+					<li><h4 style="font-size: 25px">Framework Code Igniter</h4></li>
+			</div>
+	</div>
+</div>
+</div></br></br>
+</div>
+
+<!-- //education -->
+
+
+<!-- main-content -->
+	<div class="main-content">
+		<!-- gallery -->
+		<div class="gallery" id="projects">
+			<div class="w3-gallery-head">
+				<h3>My projects</h3>
+		
+					<div class="container">
+						<div class="gallery_gds">  
+				            <div class="filtr-container " >
+								<div class="col-md-4 col-ms-6 jm-item first filtr-item" data-category="1, 5" data-sort="Busy streets" >
+									<div class="jm-item-wrapper">
+										<div class="jm-item-image">
+											<img src="<?php echo base_url();?>asset/images/v.jpg" alt="property" />
+											<p>Tes</p>
+											<span class="jm-item-overlay"> </span>
+											<div class="jm-item-button"><a href="#"  data-toggle="modal" data-target="#myModal1">View Details</a></div>
+										</div>	
+										
+									</div>
+								</div>
+								<div class="col-md-4 col-ms-6 jm-item first filtr-item" data-category="1, 5" data-sort="Busy streets" >
+									<div class="jm-item-wrapper">
+										<div class="jm-item-image">
+											<img src="<?php echo base_url();?>asset/images/tr.jpg" alt="property" />
+											<span class="jm-item-overlay"> </span>
+											<div class="jm-item-button"><a href="#"  data-toggle="modal" data-target="#myModal2">View Details</a></div>
+										</div>	
+										
+									</div>
+								</div>
+								
+				            </div>
+						</div>
+					</div>	
+			</div>
+		</div>
+	</div>
+	<!--//gallery-->
+	
+<!-- //main-content -->			 
+<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" >
+				<div class="modal-dialog">
+							<!-- Modal content-->
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+							<div class="w3ls-property-images">
+								<img src="<?php echo base_url();?>asset/images/vo.jpg" alt="image">
+							</div>
+					
+						<div class="ins-details">
+							<div class="ins-name">
+								<h3>VolunteerKu</h3>
+								<p style="text-indent: 0.5in" align="justify">VolunteerKu adalah aplikasi berbasis web yang dibuat guna melengkapi syarat untuk mencapai jenjang setara sarjana muda Universitas Gunadarma di semester 6 menggunakan framework Laravel. VolunteerKu merupakan media pendaftaran bagi calon volunteer dan penyelenggara kegiatan. Jika penyelenggara kegiatan membutuhkan tenaga tambahan, dapat mendaftarkan kegiatannya di website ini. Kemudian penyelenggara kegiatan menerima data calon volunteer yang ingin berpartisipasi pada kegiatannya. Sedangkan bagi volunteer yang ingin berpartisipasi dalam suatu kegiatan, dapat mendaftarkan diri dengan memilih kegiatan mana yang ingin diikuti. </p>
+								
+							</div>
+							
+						</div>
+						<div class="w3ls-property-images">
+								<img src="<?php echo base_url();?>asset/images/vo2.jpg" alt="image">
+							</div>
+						<div class="clearfix"></div>			
+			     </div>
+	</div>
+
+	<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" >
+				<div class="modal-dialog">
+							<!-- Modal content-->
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+							<div class="w3ls-property-images">
+								<img src="<?php echo base_url();?>asset/images/t.jpg" alt="image">
+							</div>
+					
+						<div class="ins-details">
+							<div class="ins-name">
+								<h3>Penida Tour Bali</h3>
+								<p style="text-indent: 0.5in" align="justify">Penida Island Tour merupakan website travel yang dikhususkan untuk pariwisata pulau Nusa Penida Bali. Pada website tersebut dapat memesan paket tour. Terdapat halaman admin untuk mengatur informasi yang ditampilkan pada website. Dapat diakses <a rel="stylesheet" type="text/css" href="http://penidatourbali.com">disini</a> </p>
+								
+							</div>
+							
+						</div>
+						<div class="w3ls-property-images">
+								<img src="<?php echo base_url();?>asset/images/tr1.jpg" alt="image">
+							</div>
+						<div class="clearfix"></div>			
+			     </div>
+	</div>
+ <script src="js/jquery.filterizr.js"></script>
+    
+    <!-- Kick off Filterizr -->
+    <script type="text/javascript">
+        $(function() {
+            //Initialize filterizr with default options
+            $('.filtr-container').filterizr();
+        });
+    </script>
+
+<!-- contact -->
+	 <div class="contact" id="contact">
+	<div class="container">
+		<div class="w3ls-heading">
+			<h3 style="color: white;font-size: 30px">Contact me</h3>
+		</div>
+			<div class="col-md-4 w3ls_footer_grid_left">
+						<div class="w3ls_footer_grid_leftl">
+							<i class="fa fa-phone"></i>
+						</div>
+						<div class="w3ls_footer_grid_leftr">
+							<h4 align="center">Phone</h4>
+							<p align="center" style="color: white;font-size: 17px"><b>+62856-8583-834</b></p>
+						</div>
+						<div class="clearfix"> </div>
+			</div>
+			<div class="col-md-4 w3ls_footer_grid_left">
+						<div class="w3ls_footer_grid_leftl">
+							<i class="fa fa-globe"></i>
+						</div>
+						<div class="w3ls_footer_grid_leftr">
+							<h4 align="center">Blog</h4>
+							<p align="center" ><a rel="stylesheet" type="text/css" href="http://yulistny.blogspot.co.id" style="color: white;font-size: 17px"><b>yulistny.blogspot.co.id</b></a></p>
+						</div>
+						<div class="clearfix"> </div>
+			</div>
+			<div class="col-md-4 w3ls_footer_grid_left">
+						<div class="w3ls_footer_grid_leftl">
+							<i class="fa fa-file"></i>
+						</div>
+						<div class="w3ls_footer_grid_leftr">
+							<h4 align="center">Email</h4>
+							<p align="center" style="color: white;font-size: 17px"><b>yulistny@gmail.com</b></p>
+						</div>
+						<div class="clearfix"> </div>
+			</div>
+			<div class="col-md-4 w3ls_footer_grid_left">
+						<div class="w3ls_footer_grid_leftl">
+							<i class="fa fa-twitter"></i>
+						</div>
+						<div class="w3ls_footer_grid_leftr">
+							<h4 align="center">Twitter</h4>
+							<p align="center" style="color: white"><a rel="stylesheet" type="text/css" href="https://twitter.com/Yulistiaani" style="color: white;font-size: 17px"><b>Yulistiaani</b></a></p>
+						</div>
+						<div class="clearfix"> </div>
+			</div>
+			<div class="col-md-4 w3ls_footer_grid_left">
+						<div class="w3ls_footer_grid_leftl">
+							<i class="fa fa-instagram"></i>
+						</div>
+						<div class="w3ls_footer_grid_leftr">
+							<h4 align="center">Instagram</h4>
+							<p align="center" style="color: white"><a rel="stylesheet" type="text/css" href="https://www.instagram.com/yulistiaani/" style="color: white;font-size: 17px;"><b>Yulistiaani</b></a></p>
+						</div>
+						<div class="clearfix"> </div>
+			</div>
+	</div>
+	</br></br></br>
+	<p align="center" style="color: white">© 2017 C-Resume. All Rights Reserved | Design by <a href="https://w3layouts.com/">W3layouts</a></p>
+</div>
+<!-- //contact -->
+
+
+
+<script src="<?php echo base_url();?>asset/js/bars.js"></script>
+<!-- start-smoth-scrolling -->
+<script src="<?php echo base_url();?>asset/js/SmoothScroll.min.js"></script>
+<!-- text-effect -->
+		<script type="text/javascript" src="<?php echo base_url();?>asset/js/jquery.transit.js"></script> 
+		<script type="text/javascript" src="<?php echo base_url();?>asset/js/jquery.textFx.js"></script> 
+		<script type="text/javascript">
+			$(document).ready(function() {
+					$('.test').textFx({
+						type: 'fadeIn',
+						iChar: 100,
+						iAnim: 1000
+					});
+			});
+		</script>
+<!-- //text-effect -->
+<!-- menu-js --> 	
+	<script src="<?php echo base_url();?>asset/js/modernizr.js"></script>	
+	<script src="<?php echo base_url();?>asset/js/menu.js"></script>
+<!-- //menu-js --> 	
+
+
+<script type="text/javascript" src="<?php echo base_url();?>asset/js/move-top.js"></script>
+
+<script type="text/javascript" src="<?php echo base_url();?>asset/js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+
+
+</body>
+</html>
